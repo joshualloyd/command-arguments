ac9394eaee7e249447856789e55c0e995a4153fe
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-08-25 08:52:11 -0500

    now handles no arguments

--- args.js
+++ args.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 
-let args = process.argv.slice(2);
+if (!process.argv[2]) {
+    console.log(0);
+} else {
+    let args = process.argv.slice(2);
+    
+    const sum = args.filter((arg) => {return !isNaN(arg)}).map((arg) => {return Number(arg)}).reduce((a, b) => {
+        return a + b;
+    });
+    
+    console.log(sum);
+}
 
-const sum = args.filter((arg) => {return !isNaN(arg)}).map((arg) => {return Number(arg)}).reduce((a, b) => {
-    return a + b;
-});
-
-console.log(sum);
